029136668c5803fe6838f6ff68cdbd71787f1d57
geoff <geoff.newman@immersive.video>
2021-03-21 01:09:04 +1100

    EOD 20210320 - 2D space Shooter

--- 2D Shooter/Assets/Scripts/Player.cs
+++ 2D Shooter/Assets/Scripts/Player.cs
@@ -22,44 +22,7 @@ public class Player : MonoBehaviour
 	
     void Update()
     {
-       
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 dir = new Vector3(horizontalInput, verticalInput, 0);
-        // check if the controls on the horizontal and vertical axis are pressed.
-        // if right key move +x or -x if left key
-        // if the key down move -y and if up key move +y 
-        transform.Translate(dir * m_speed * Time.deltaTime);
-        // clamp the the player to the top and bottom of the screen
-        // if at bottom
-        if (transform.position.y < -4.5f )
-        {
-            // make the pleyer y value 4.5f
-            transform.position = new Vector2(transform.position.x, -4.5f);
-        }
-        // if the postion is greater than 4.5f
-        else if(transform.position.y > 4.5f)
-        {
-            // clamp the y postion to 4.5f
-            transform.position = new Vector2(transform.position.x, 4.5f);
-        }
-
-        // make the player wrap around to the other side if the player goes 
-        // hoizontally off the screen
-        // gone out of the right hand side 
-        if (transform.position.x > 11)
-        {
-            //move tot the LHS
-            transform.position = new Vector2(-11, transform.position.y);
-        }
-        // if the player goes out of the LHS
-        else if(transform.position.x < -11)
-        {
-            // Move to the RHS
-            transform.position = new Vector2(11, transform.position.y);
-        }
-        
-
+        CalcMovement();
     }
     #endregion
     #region Public Methods
@@ -68,7 +31,30 @@ public class Player : MonoBehaviour
     #endregion
     #region Private Methods
     // Place your public methods here
+    void CalcMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 dir = new Vector3(horizontalInput, verticalInput, 0);
 
+        transform.Translate(dir * m_speed * Time.deltaTime);
+
+        transform.position = new Vector2(transform.position.x,
+                                        Mathf.Clamp(transform.position.y, -4.5f, 1.5f));
+
+        if (transform.position.x > 11.3f)
+        {
+            //move to the LHS
+            transform.position = new Vector2(-11.3f, transform.position.y);
+        }
+        // if the player goes out of the LHS
+        else if (transform.position.x < -11.3f)
+        {
+            // Move to the RHS
+            transform.position = new Vector2(11.3f, transform.position.y);
+        }
+    }
     #endregion
 
 }
