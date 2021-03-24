50f8bdd8668e141f1d1cb0fbdece1f9a7bcb97ba
geoff <geoff.newman@immersive.video>
2021-03-24 15:41:35 +1100

    despawning enemey

--- 2D Shooter/Assets/Scripts/Laser.cs
+++ 2D Shooter/Assets/Scripts/Laser.cs
@@ -21,5 +21,4 @@ public class Laser : MonoBehaviour
             Destroy(this.gameObject);
 
     }
-
 }
---
+++ 2D Shooter/Assets/Scripts/enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    #region Public
+    //public members go here
+
+    #endregion
+
+    #region Private
+    //private members go here
+    [SerializeField] float m_speed = 4.0f;
+  #endregion
+  // Place all unity Message Methods here like OnCollision, Update, Start ect. 
+  #region Unity Messages 
+    void Start()
+    {
+		
+    }
+	
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "playerProjectile")
+        {
+            Spawn();
+        }
+    }
+    #endregion
+    #region Public Methods
+    //Place your public methods here
+    private void UpdateMovement()
+    {
+        transform.Translate(Vector3.down * m_speed * Time.deltaTime);
+        if (transform.position.y < -7f)
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        float randomX = Random.Range(-8, 8f);
+        transform.position = new Vector3(randomX, 7, 0);
+    }
+
+    #endregion
+    #region Private Methods
+    //Place your public methods here
+
+    #endregion
+
+}
