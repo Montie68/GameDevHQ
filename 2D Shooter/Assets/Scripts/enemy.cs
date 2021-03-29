00354eb59daff7c7c3188d809343d7f52a450251
geoff <geoff@dropbeargames.com.au>
2021-03-29 23:14:24 +1100

    EOD 20210329

--- 2D Shooter/Assets/Scripts/enemy.cs
+++ 2D Shooter/Assets/Scripts/enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
 
     public int m_DoesDamage = 25;
     public int m_health = 10;
+
+    [SerializeField] float m_speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,25 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.down * m_speed * Time.deltaTime);
 
+        if (transform.position.y < -9.0f)
+        {
+            transform.position = new Vector3(Random.Range(-8f,8f), 9f);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if 
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Player>().m_health -= m_DoesDamage;
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
     }
 }
