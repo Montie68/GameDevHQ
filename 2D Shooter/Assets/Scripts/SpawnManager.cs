3a0b083ce9ab66d9749f934bb6102d2c12064d41
geoff <geoff@dropbeargames.com.au>
2021-06-21 23:47:47 +1000

    clean up

--- 2D Shooter/Assets/Scripts/SpawnManager.cs
+++ 2D Shooter/Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,15 @@ public class SpawnManager : MonoBehaviour
     private Float2 m_spawnRange = new Float2(0, 5);
     [SerializeField]
     private GameObject m_enemyPrefab;
+    // Game Object Container as the parent for the enemy prefabs
+    [SerializeField]
+    private Transform m_enemyObjectContainer;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnEnemy());
+        if (m_enemyObjectContainer == null) m_enemyObjectContainer = this.transform;
     }
 
     // Update is called once per frame
@@ -26,6 +31,7 @@ public class SpawnManager : MonoBehaviour
         {
             Vector3 pos = new Vector3( Random.Range(-8.0f, 8.0f), 9);
             GameObject _enemy = Instantiate(m_enemyPrefab, pos, Quaternion.identity);
+            _enemy.transform.parent = m_enemyObjectContainer;
             yield return new WaitForSeconds(Random.Range(m_spawnRange.a, m_spawnRange.b));
         }
     }
